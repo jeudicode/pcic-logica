6292aba69ef48114987fa0430fa77ccd458e4ecc
Diego Isla <dislalopez@gmail.com>
2020-05-26 14:43:54 -0500

    Main file

--- proyecto/main.py
+++ proyecto/main.py
@@ -8,33 +8,30 @@ if len(sys.argv) == 5:
     op = sys.argv[3]
     order = sys.argv[4]
 
-    # if f1[0] not in operators:
-    #     sys.exit("Invalid formula")
-    # else:
-    #     for (i, sym) in enumerate(f1):
-    #         print(sym)
-
     b1 = BDD(f1, order)
-    print("Formula for B1: ", b1.formula)
+    print("\nFormula for B1: ", b1.formula)
     print("\nOriginal B1:\n")
     b1._print()
-    # print(b1.a)
+    print("nodes in b1: ", b1.node_count)
     b2 = BDD(f2, order)
-    print("Formula for B2: ", b2.formula)
+    print("\nFormula for B2: ", b2.formula)
     print("\nOriginal B2:\n")
     b2._print()
-    b1._reduce()
+    print("nodes in b1: ", b1.node_count)
     print("\nReducing B1:\n")
+    b1._reduce()
+    c1 = b1.count_nodes()
     b1._print()
+    print("nodes in b1: ", c1)
     print("\nReducing B2:\n")
     b2._reduce()
+    c2 = b2.count_nodes()
     b2._print()
-    # print(b2.a)
+    print("nodes in b2: ", c2)
     print("\nApplying B1 " + op + " B2:\n")
     result = BDD(None, None, b1.apply(b1.root, b2.root, op))
     result._reduce()
     result._print()
+
 else:
     sys.exit("Not enough arguments.")
-
-# +(p,+(qr))
